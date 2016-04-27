d4a0d804cb935cf9f7f030838c4f8ba27e252aab
Nickolay Baidyukov <n.baidukov@argustelecom.ru>
2016-04-27 17:39:07 +0300

    Исправление пути к jar файлу

--- common.sh
+++ common.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 
 ###############################################################################
+antlr='antlr-4.5.2-complete.jar'
+###############################################################################
 grammar=PlSql.g4
-src_path=src/Antlr4ParserLexer/src/org/antlr/generated
-grammar_class=org.antlr.generated.PlSql
 start_rule=sql_script
+grammar_class=org.antlr.generated.PlSql
+###############################################################################
+src_path=./src/Antlr4ParserLexer/src/org/antlr/generated
+jar_path=./src/Antlr4ParserLexer/lib/$antlr
 ###############################################################################
 
 #  [-tokens] [-tree] [-gui] [-trace]
 
-antlr='antlr-4.5.2-complete.jar'
 # java_options='-client -Xms100m -Xmx100m'
 java_options=
 
-export CLASSPATH="./internal_class:./jars/$antlr:$CLASSPATH"
+export CLASSPATH="./internal_class:$jar_path:$CLASSPATH"
 #alias grun='java $java_options org.antlr.v4.gui.TestRig'
 alias runTest='java $java_options org.antlr.v4.gui.TestRig $grammar_class $start_rule'
 alias antlr4='java $java_options org.antlr.v4.Tool'
